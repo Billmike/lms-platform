cd4bcb53e43581572741e22fe087660350513338
Billmike <ayelegun.kayode@gmail.com>
2024-12-08 17:17:59 +0000

    feat: add proper check for the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,23 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
+import { jwt } from './lib/auth';
 
 export async function middleware(request: NextRequest) {
   const cookiesStore = (await cookies()).get('lms_auth_token');
 
   if (cookiesStore) {
+    const payload = jwt.verify(cookiesStore?.value);
+    const pathname = request.nextUrl.pathname;
+
+    if (pathname === '/dashboard/admin' && payload?.role !== 'admin') {
+      return NextResponse.redirect(new URL(`/dashboard/${payload?.role}`, request.url))
+    }
+
+    if (pathname === '/dashboard/instructor' && payload?.role === 'student') {
+      return NextResponse.redirect(new URL('/dashboard/student', request.url))
+    }
+
     return NextResponse.next();
   }
 

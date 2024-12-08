b51ca0231728c9766b0baacba54b70d68279c299
Billmike <ayelegun.kayode@gmail.com>
2024-12-08 17:33:16 +0000

    chore: fix build

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
+export async function POST() {
   try {
     const cookiesStore = await cookies();
 
